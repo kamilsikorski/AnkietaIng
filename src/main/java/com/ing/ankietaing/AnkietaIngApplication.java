1bf2a307327309559bbb87c18c214c7395b38da3
sikorski <kamilsikorski@yahoo.com>
2022-10-09 11:07:46 +0200

    Initial commit

---
+++ src/main/java/com/ing/ankietaing/AnkietaIngApplication.java
@@ -0,0 +1,13 @@
+package com.ing.ankietaing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkietaIngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnkietaIngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ing/ankietaing/AnkietaIngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.ankietaing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkietaIngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
